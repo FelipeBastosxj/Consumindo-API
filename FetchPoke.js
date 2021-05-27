f60fdc5e2ae59b3b4f1369c3fd10ae751db838f2
FelipeBastosxj <felipesi0305@gmail.com>
2021-05-27 16:30:39 -0300

    primeiro commit

---
+++ FetchPoke.js
@@ -0,0 +1,7 @@
+loading = document.querySelector('.loading')
+estrutura = document.querySelector('.estrutura')
+
+setTimeout(e =>{
+    loading.style.display = 'none'
+    estrutura.style.display = 'block'
+},2000)
