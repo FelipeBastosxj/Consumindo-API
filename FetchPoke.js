483d74748a85a568ab606fc65f39f14ea02d8e5f
FelipeBastosxj <felipesi0305@gmail.com>
2021-05-27 18:22:47 -0300

    Listando

--- FetchPoke.js
+++ FetchPoke.js
@@ -5,3 +5,51 @@ setTimeout(e =>{
     loading.style.display = 'none'
     estrutura.style.display = 'block'
 },2000)
+
+async function fetchAPI (){
+    try {
+
+    const resposta = await fetch('https://pokeapi.co/api/v2/pokemon?limit=107&offset=386')
+    const ListPoke = await resposta.json()
+    listar(ListPoke)
+
+ }catch(erro){
+     console.log(erro)
+ }
+}
+
+
+
+function listar(ListPoke){
+try{
+
+    ListPoke.results.forEach(e =>{
+
+   return fetch(e.url)
+        .then(r => r.json())
+        .then(p => dom(p))
+    })
+
+}catch(erro){
+    console.log(erro)
+}
+}
+
+
+
+function dom(p){
+ adicionar = document.querySelector('.pokemons-list')
+ div = document.createElement('div')
+
+ div.innerHTML = `<div class= card>
+ <img src="${p.sprites.front_default}" alt="">
+ <button class="nomep">
+    ${p.name}
+ </button>
+</div>`
+
+adicionar.appendChild(div)
+}
+
+
+fetchAPI()
