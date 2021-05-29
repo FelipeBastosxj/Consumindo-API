dc4f910e3ae4f5e4779d6b45382ca596ee60a682
FelipeBastosxj <felipesi0305@gmail.com>
2021-05-29 17:07:41 -0300

    status

--- FetchPoke.js
+++ FetchPoke.js
@@ -80,27 +80,59 @@ function modalInsert(pokemon){
   
   modal.style.display ='flex'
 
+  MOVE[0].innerHTML = 'MOVE1:' + ' ' + pokemon.moves[16].move.name
+  MOVE[1].innerHTML = 'MOVE2:' + ' ' + pokemon.moves[17].move.name
+  MOVE[2].innerHTML = 'MOVE3:' + ' ' + pokemon.moves[18].move.name
+  MOVE[3].innerHTML = 'MOVE4:' + ' ' + pokemon.moves[19].move.name
+  MOVE[4].innerHTML = 'MOVE5:' + ' ' + pokemon.moves[20].move.name
+  MOVE[5].innerHTML = 'MOVE6:' + ' ' + pokemon.moves[21].move.name
+  HP.innerHTML = 'HP:' + ' ' + pokemon.stats[0].base_stat
+  ATTACK.innerHTML = 'ATTACK:' + ' ' + pokemon.stats[1].base_stat
+  DEFENSE.innerHTML = 'DEFENSE:' + ' ' + pokemon.stats[2].base_stat
+  SPECIALATACK.innerHTML = 'SPECIAL-ATACK:' + ' ' + pokemon.stats[3].base_stat
+  SPECIALDEFENSE.innerHTML = 'SPECIAL-ATACK:' + ' ' + pokemon.stats[4].base_stat
+  SPEED.innerHTML = 'SPEED:' + ' '+ pokemon.stats[5].base_stat
   titulop.innerHTML = `${pokemon.name}`  
   foto.innerHTML = `<img src="${pokemon.sprites.front_default}" alt=""></img>`
   
 }
 
 let modal = document.querySelector('.modal')
-fecharmodal = document.querySelector('.fmodal')
+let fecharmodal = document.querySelector('.fmodal')
 
 fecharmodal.addEventListener('click', () =>{
     modal.style.display = 'none'
 })
 
-capiturar = document.querySelector('.capiturar')
-achar = document.querySelector('.achar')
-
+let capiturar = document.querySelector('.capiturar')
+let achar = document.querySelector('.achar')
+let HP = document.querySelector('.HP')
+let ATTACK = document.querySelector('.ATTACK')
+let DEFENSE = document.querySelector('.DEFENSE')
+let SPECIALATACK = document.querySelector('.SPECIAL-ATACK')
+let SPECIALDEFENSE = document.querySelector('.SPECIAL-DEFENSE')
+let SPEED = document.querySelector('.SPEED')
+let MOVE = document.querySelectorAll('.MOVE')
 async function capitura(){
     
     const resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${achar.value[0].toLowerCase() + achar.value.substr(1)}`)
 
     if(resposta.ok){
         const pokemon = await resposta.json()
+        console.log(pokemon)
+
+        MOVE[0].innerHTML = 'MOVE1:' + ' ' + pokemon.moves[16].move.name
+        MOVE[1].innerHTML = 'MOVE2:' + ' ' + pokemon.moves[17].move.name
+        MOVE[2].innerHTML = 'MOVE3:' + ' ' + pokemon.moves[18].move.name
+        MOVE[3].innerHTML = 'MOVE4:' + ' ' + pokemon.moves[19].move.name
+        MOVE[4].innerHTML = 'MOVE5:' + ' ' + pokemon.moves[20].move.name
+        MOVE[5].innerHTML = 'MOVE6:' + ' ' + pokemon.moves[21].move.name
+        HP.innerHTML = 'HP:' + ' ' + pokemon.stats[0].base_stat
+        ATTACK.innerHTML = 'ATTACK:' + ' ' + pokemon.stats[1].base_stat
+        DEFENSE.innerHTML = 'DEFENSE:' + ' ' + pokemon.stats[2].base_stat
+        SPECIALATACK.innerHTML = 'SPECIAL-ATACK:' + ' ' + pokemon.stats[3].base_stat
+        SPECIALDEFENSE.innerHTML = 'SPECIAL-ATACK:' + ' ' + pokemon.stats[4].base_stat
+        SPEED.innerHTML = 'SPEED:' + ' '+ pokemon.stats[5].base_stat
         modal.style.display ='flex'
         titulop.innerHTML = `${pokemon.name}`  
         foto.innerHTML = `<img src="${pokemon.sprites.front_default}" alt=""></img>`
